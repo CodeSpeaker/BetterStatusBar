ef97134c0b6ce034648a8c2f85e323cee78b23d2
GitHub Action <action@github.com>
2021-12-09 09:04:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/codespeaker/betterstatusbar/services/MyProjectService.kt
+++ src/main/kotlin/com/github/codespeaker/betterstatusbar/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.codespeaker.betterstatusbar.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.codespeaker.betterstatusbar.MyBundle
 
 class MyProjectService(project: Project) {
 
