094f6d087b275c6bf1c445c34ed0f166fb82cab8
wuchao26 <wuchao26@gome.com.cn>
2022-04-11 20:24:38 +0800

    添加切换年月的功能

--- src/main/java/betterstatusbar/status/components/CalendarGridPanel.java
+++ src/main/java/betterstatusbar/status/components/CalendarGridPanel.java
@@ -4,6 +4,7 @@ import betterstatusbar.status.data.BaiduCalendarData;
 import betterstatusbar.status.util.GridConstraintsUtil;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
@@ -19,16 +20,21 @@ import com.intellij.util.io.HttpRequests;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.xml.ui.TextPanel;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.groovy.util.Maps;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.font.TextAttribute;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
@@ -36,6 +42,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.locks.StampedLock;
 import java.util.stream.Collectors;
 
 class CalendarGridPanel extends OpaquePanel implements Disposable {
@@ -47,6 +54,15 @@ class CalendarGridPanel extends OpaquePanel implements Disposable {
     private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
     private static final String CALENDAR_URL_FORMAT = "https://sp1.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?tn=wisetpl&resource_id=39043&query=%d年%d月";
     private static final JsonMapper JSON_MAPPER = new JsonMapper();
+
+    private ZonedDateTime now = ZonedDateTime.now(ZONE_ID).truncatedTo(ChronoUnit.DAYS);
+    private ZonedDateTime curMonth = ZonedDateTime.now(ZONE_ID).truncatedTo(ChronoUnit.DAYS);
+    private StampedLock lock = new StampedLock();
+    private JBLabel preYear;
+    private JBLabel preMonth;
+    private JBLabel curYearMonth;
+    private JBLabel nextMonth;
+    private JBLabel nextYear;
 
     static {
         STATUS_MAP.put("1", CalBorder.REST_DAY.border);
@@ -61,19 +77,36 @@ class CalendarGridPanel extends OpaquePanel implements Disposable {
     }
 
     CalendarGridPanel(){
-        ZonedDateTime now = ZonedDateTime.now(ZONE_ID).truncatedTo(ChronoUnit.DAYS);
         Future<Map<ZonedDateTime, BaiduCalendarData>> dateMapFuture = getDateMapFuture(now.getYear(), now.getMonth().getValue());
         ZonedDateTime curDate = now.withDayOfMonth(1).minusWeeks(1).with(ChronoField.DAY_OF_WEEK, 7);
 
-        setLayout(new GridLayoutManager(8, 7, JBUI.insets(1), 2, 2));
+        setLayout(new GridLayoutManager(9, 7, JBUI.insets(1), 2, 2));
 
         DateTimePanel dateTimePanel = new DateTimePanel();
         Disposer.register(this, dateTimePanel);
         add(dateTimePanel, GridConstraintsUtil.getPositionGridConstraints(0, 0, 7, 100, 160));
 
+        preYear = new JBLabel(AllIcons.Diff.Arrow);
+        preMonth = new JBLabel(AllIcons.Actions.ArrowCollapse);
+        curYearMonth = new JBLabel(curMonth.format(DateTimeFormatter.ofPattern("yyyy-MM")));
+        curYearMonth.setFont(Font.getFont(Maps.of(TextAttribute.SIZE, 15)));
+        nextMonth = new JBLabel(AllIcons.Actions.ArrowExpand);
+        nextYear = new JBLabel(AllIcons.Diff.ArrowRight);
+        add(preYear, GridConstraintsUtil.getPositionGridConstraints(1, 1));
+        add(preMonth, GridConstraintsUtil.getPositionGridConstraints(1, 2));
+        add(curYearMonth, GridConstraintsUtil.getPositionGridConstraints(1, 3));
+        add(nextMonth, GridConstraintsUtil.getPositionGridConstraints(1, 4));
+        add(nextYear, GridConstraintsUtil.getPositionGridConstraints(1, 5));
+
+        preYear.addMouseListener(new MouseListener(-1, 0));
+        preMonth.addMouseListener(new MouseListener(0, -1));
+        curYearMonth.addMouseWheelListener(new MouseListener());
+        nextMonth.addMouseListener(new MouseListener(0, 1));
+        nextYear.addMouseListener(new MouseListener(1, 0));
+
         for (int i = 0; i < 7; i++) {
             JBLabel weekdayLabel = new JBLabel(WEEKDAYS[i]);
-            add(weekdayLabel, GridConstraintsUtil.getPositionGridConstraints(1, i % 7, -1, 0, 50));
+            add(weekdayLabel, GridConstraintsUtil.getPositionGridConstraints(2, i % 7, -1, 0, 50));
         }
 
         Map<ZonedDateTime, BaiduCalendarData> dateMap;
@@ -93,11 +126,12 @@ class CalendarGridPanel extends OpaquePanel implements Disposable {
                 panel.setLabelForeground(new JBColor(0, 0));
                 dateTimePanel.setInfoText(tempData.suit, tempData.avoid);
             }
-            add(panel, GridConstraintsUtil.getPositionGridConstraints(i / 7 + 2, i % 7, -1, 100, 100));
+            add(panel, GridConstraintsUtil.getPositionGridConstraints(i / 7 + 3, i % 7, -1, 100, 100));
             curDate = curDate.plusDays(1);
         }
 
         this.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        this.addMouseWheelListener(new MouseListener());
     }
 
     /**
@@ -110,8 +144,9 @@ class CalendarGridPanel extends OpaquePanel implements Disposable {
      */
     private Border getBorder(ZonedDateTime now, ZonedDateTime curDate, String status) {
         boolean isCurrentMonth = now.getMonth().equals(curDate.getMonth());
+        boolean isCurrentYear = now.getYear() == curDate.getYear();
         boolean isWeekend = curDate.get(ChronoField.DAY_OF_WEEK) == 6 || curDate.get(ChronoField.DAY_OF_WEEK) == 7;
-        return STATUS_MAP.getOrDefault(status, BORDER_RULE_MAP.get(Pair.create(isCurrentMonth, isWeekend)));
+        return STATUS_MAP.getOrDefault(status, BORDER_RULE_MAP.get(Pair.create(isCurrentMonth && isCurrentYear, isWeekend)));
     }
 
     @NotNull
@@ -246,6 +281,80 @@ class CalendarGridPanel extends OpaquePanel implements Disposable {
 
         CalBorder(Border border){
             this.border = border;
+        }
+    }
+
+    private class MouseListener extends MouseAdapter {
+
+        int year;
+        int month;
+
+        MouseListener(int year, int month) {
+            this.year = year;
+            this.month = month;
+        }
+
+        MouseListener() {
+
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            curMonth = curMonth.plusYears(year);
+            curMonth = curMonth.plusMonths(month);
+            changeText();
+        }
+
+        private void changeText() {
+            curYearMonth.setText(curMonth.format(DateTimeFormatter.ofPattern("yyyy-MM")));
+            new Thread(() -> {
+                long stamp = lock.tryWriteLock();
+                try {
+                    if (stamp == 0L) {
+                        return;
+                    }
+                    Thread.sleep(300);
+                    Future<Map<ZonedDateTime, BaiduCalendarData>> dateMapFuture = getDateMapFuture(curMonth.getYear(), curMonth.getMonth().getValue());
+                    ZonedDateTime curDate = curMonth.withDayOfMonth(1).minusWeeks(1).with(ChronoField.DAY_OF_WEEK, 7);
+
+                    Map<ZonedDateTime, BaiduCalendarData> dateMap;
+                    try {
+                        dateMap = dateMapFuture.get();
+                    } catch (Exception e) {
+                        dateMap = new HashMap<>();
+                    }
+
+                    for(int i = 0; i < 42; ++i) {
+                        BaiduCalendarData tempData = dateMap.getOrDefault(curDate, BaiduCalendarData.DEFAULT);
+                        DateNumPanel panel = new DateNumPanel(String.valueOf(curDate.getDayOfMonth()), tempData);
+
+                        panel.setBorder(getBorder(now, curDate, tempData.status));
+                        if (now.equals(curDate)) {
+                            panel.setBackground(new JBColor(0XFF6400, 0XFF6400));
+                            panel.setLabelForeground(new JBColor(0, 0));
+                        }
+                        add(panel, GridConstraintsUtil.getPositionGridConstraints(i / 7 + 3, i % 7, -1, 100, 100));
+                        curDate = curDate.plusDays(1);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (stamp != 0) {
+                        lock.unlockWrite(stamp);
+                    }
+                }
+            }).start();
+
+        }
+
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent event) {
+            if (InputEvent.getModifiersExText(event.getModifiersEx()).equals(Toolkit.getProperty("AWT.shift", "Shift"))) {
+                curMonth = curMonth.plusYears(event.getWheelRotation());
+            } else {
+                curMonth = curMonth.plusMonths(event.getWheelRotation());
+            }
+            changeText();
         }
     }
 }
--- src/main/java/betterstatusbar/status/util/GridConstraintsUtil.java
+++ src/main/java/betterstatusbar/status/util/GridConstraintsUtil.java
@@ -11,6 +11,21 @@ public class GridConstraintsUtil {
         return gridConstraints;
     }
 
+    public static GridConstraints getPositionGridConstraints(int row, int column, int colSpan, int rowSpan) {
+        GridConstraints gridConstraints = new GridConstraints();
+        gridConstraints.setRow(row);
+        gridConstraints.setColumn(column);
+
+        if (colSpan != -1) {
+            gridConstraints.setColSpan(colSpan);
+        }
+
+        if (rowSpan != -1) {
+            gridConstraints.setRowSpan(rowSpan);
+        }
+        return gridConstraints;
+    }
+
     public static GridConstraints getPositionGridConstraints(int row, int column, int colSpan, int width, int height) {
         GridConstraints gridConstraints = new GridConstraints();
         gridConstraints.setRow(row);
